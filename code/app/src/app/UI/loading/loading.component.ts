9cf97e99c62c00789ccfa0c0709077a029867f06
Marco Monsanto <marco.monsanto09@gmail.com>
2019-01-29 15:06:30 +0000

    added next 5 games in user-info
    Former-commit-id: 7a4e02a426828e05705c60bbf160fae7959cb5ce

---
+++ code/app/src/app/UI/loading/loading.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'ui-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.scss']
+})
+export class LoadingComponent implements OnInit {
+
+  @Input() visible = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
